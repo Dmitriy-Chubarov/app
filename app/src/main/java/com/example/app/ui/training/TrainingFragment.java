024c55fa2012d715b068ebd1317ffd00b0c3e547
Churina Margarita <elshanskaya.r@inbox.ru>
2024-04-01 13:23:16 +0300

    wokout activity made

--- app/src/main/java/com/example/app/ui/training/TrainingFragment.java
+++ app/src/main/java/com/example/app/ui/training/TrainingFragment.java
@@ -1,5 +1,8 @@
 package com.example.app.ui.training;
 
+import static com.example.app.R.id.button2;
+
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -16,6 +20,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.app.R;
+import com.example.app.WorkoutActivity;
 import com.example.app.databinding.FragmentTrainingBinding;
 
 public class TrainingFragment extends Fragment {
@@ -25,22 +30,10 @@ public class TrainingFragment extends Fragment {
     private String[] levels = {"Легкий", "Умеренный", "Интенсивный"};
     Spinner type1;
     Spinner level1;
+    Button btnStartTraining;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        TrainingViewModel trainingViewModel =
-//                new ViewModelProvider(this).get(TrainingViewModel.class);
-//
-//        binding = FragmentTrainingBinding.inflate(inflater, container, false);
-//        View root = binding.getRoot();
-//
-//        View v = inflater.inflate(R.layout.fragment_training, container, false);
-//        Spinner spinner = (Spinner) v.findViewById(R.id.type);
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(),
-//                android.R.layout.simple_spinner_item, types);
-//        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-//        spinner.setAdapter(adapter);
-
         View rootView = inflater.inflate(R.layout.fragment_training, container, false);
 
         type1 = rootView.findViewById(R.id.type);
@@ -49,10 +42,21 @@ public class TrainingFragment extends Fragment {
         level1 = rootView.findViewById(R.id.level);
         initlevelspinnerfooter();
 
-        return rootView;
+        btnStartTraining = (Button) rootView.findViewById(button2);
+        View.OnClickListener oclBtnOk = (new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        //return root;
+                Intent intent = new Intent(getActivity(), WorkoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStartTraining.setOnClickListener(oclBtnOk);
+
+        return rootView;
     }
+
 
     private void inittypespinnerfooter() {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
