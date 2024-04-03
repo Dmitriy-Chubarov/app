45f27ccb2aa9a6dc4bbd7934ccb475b1baad5d24
Авдышева Полина <polinchikavlove@gmail.com>
2024-04-04 00:05:22 +0300

    добавила столбчатую диаграмму и пофиксила цвета

--- app/src/main/java/com/example/app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/app/ui/home/HomeFragment.java
@@ -14,7 +14,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.app.databinding.FragmentHomeBinding;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -33,12 +37,16 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
         pieChart = (PieChart) root.findViewById(R.id.pieChart);
+
         ArrayList<PieEntry> entries = new ArrayList<>();
         entries.add(new PieEntry(25f, "Выполненные тренировки"));
         entries.add(new PieEntry(35f, "Пропущенные тренировки"));
         PieDataSet dataSet = new PieDataSet(entries, "Pie Chart");
-        dataSet.setColors(Color.BLUE, Color.GRAY);
+
+        int purple500 = 0xFF6200EE;
+        dataSet.setColors(purple500, Color.GRAY);
         dataSet.setValueTextSize(25f);
         dataSet.setValueTextColor(Color.BLACK);
         pieChart.setHoleColor(Color.TRANSPARENT);
@@ -46,6 +54,27 @@ public class HomeFragment extends Fragment {
         PieData data = new PieData(dataSet);
         pieChart.setData(data);
         pieChart.invalidate(); // refresh chart
+
+        BarChart barChart = (BarChart) root.findViewById(R.id.barChart);
+
+        ArrayList<BarEntry> entries2 = new ArrayList<>();
+        entries2.add(new BarEntry(1, 20));
+        entries2.add(new BarEntry(2, 25));
+        entries2.add(new BarEntry(3, 30));
+        entries2.add(new BarEntry(4, 5));
+        entries2.add(new BarEntry(5, 50));
+        entries2.add(new BarEntry(6, 10));
+        BarDataSet dataSet2 = new BarDataSet(entries2, "Label");
+
+        int teal = 0xFF03DAC5;
+        dataSet2.setLabel("Количество тренировок в месяц");
+        dataSet2.setColor(teal);
+        dataSet2.setValueTextColor(Color.BLACK);
+        dataSet2.setValueTextSize(15f);
+        BarData data2 = new BarData(dataSet2);
+        barChart.setData(data2);
+        barChart.invalidate();
+
         return root;
     }
 
